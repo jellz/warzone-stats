f48171f134802eeca085f94e4253704f346701c7
Daniel Gulic <danielgulic@gmail.com>
2018-12-08 15:10:25 +1100

    apiURI -> apiUri

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on('message', async (msg) => {
 
   if (msg.content.toLowerCase().startsWith(config.discordPrefix + 'player')) {
     if (!args[0]) return msg.channel.send(`**Usage!** ${config.discordPrefix}player <playername>`);
-      const response = await fetch(config.apiURI + '/mc/player/' + args[0].toLowerCase());
+      const response = await fetch(config.apiUri + '/mc/player/' + args[0].toLowerCase());
       const body = await response.json();
       if (body['notFound']) return msg.channel.send('Invalid player.');
       const embed = new MessageEmbed();
@@ -41,7 +41,7 @@ client.on('message', async (msg) => {
       // embed.addField('K/D', body.user['kills'] ? body.user['kills'] : 0 / body.user['deaths'] ? body.user['deaths'] : 0, true);
       msg.channel.send({ embed });
   } else if (msg.content.toLowerCase().startsWith(config.discordPrefix + 'leaderboard') || msg.content.toLowerCase().startsWith(config.discordPrefix + 'lb')) {
-    const response = await fetch(config.apiURI + '/mc/leaderboard/kills');
+    const response = await fetch(config.apiUri + '/mc/leaderboard/kills');
     const leaderboard = await response.json();
     var count = 0;
     const lbMsg = [];
@@ -81,7 +81,7 @@ client.on('message', async (msg) => {
     msg.channel.send('Pong!');
   } else if (msg.content.toLowerCase().startsWith(config.discordPrefix + 'punishments')) {
     try {
-      const response = await fetch(config.apiURI + '/mc/punishment/latest?limit=10');
+      const response = await fetch(config.apiUri + '/mc/punishment/latest?limit=10');
       const punishments = await response.json();
       const punMsg = [];
       var punType;
@@ -121,7 +121,7 @@ client.on('message', async (msg) => {
       embed.setColor('RED');
       msg.channel.send({ embed });
     } else if (args[0].toLowerCase() == 'game') {
-      const response = await fetch(config.apiURI + '/mc/server/stats', { method: 'POST', body: JSON.stringify({ name: 'Warzone' }), headers: { 'Content-Type': 'application/json' } });
+      const response = await fetch(config.apiUri + '/mc/server/stats', { method: 'POST', body: JSON.stringify({ name: 'Warzone' }), headers: { 'Content-Type': 'application/json' } });
       const info = await response.json();
       const embed = new MessageEmbed();
       embed.addField('IP', 'play.warzone.network', true);
