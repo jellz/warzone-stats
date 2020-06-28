66a0f6191b14680e0fcafb995a945213c031d54d
Daniel Gulic <danielgulic@gmail.com>
2020-06-29 09:38:06 +1000

    mention admins too

--- index.js
+++ index.js
@@ -96,11 +96,11 @@ client.on('message', async msg => {
 		const staff = [];
 		msg.guild.members.forEach(m => {
 			const roles = m.roles.map(r => r.name.toLowerCase());
-			if (
-				(roles.includes('moderator') || roles.includes('junior moderator')) &&
-				(m.presence.status === 'online' || m.presence.status === 'dnd')
-			)
+			if (!(m.presence.status === 'online' || m.presence.status === 'dnd'))
+				return;
+			if (roles.includes('moderator') || roles.includes('junior moderator'))
 				staff.push(m);
+			if (m.id === '142730862948122625' || '250536623270264833') staff.push(m);
 		});
 		if (staff.length < 1) return msg.reply('No online staff right now');
 		msg.channel.send(
