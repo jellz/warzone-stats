aca77aa0e4ad418676928fb6d23cd4e210fbfe8e
Daniel Gulic <danielgulic@gmail.com>
2020-06-29 09:40:08 +1000

    wrong or condition

--- index.js
+++ index.js
@@ -100,7 +100,8 @@ client.on('message', async msg => {
 				return;
 			if (roles.includes('moderator') || roles.includes('junior moderator'))
 				staff.push(m);
-			if (m.id === '142730862948122625' || '250536623270264833') staff.push(m);
+			if (m.id === '142730862948122625' || m.id === '250536623270264833')
+				staff.push(m);
 		});
 		if (staff.length < 1) return msg.reply('No online staff right now');
 		msg.channel.send(
