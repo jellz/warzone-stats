18141c5a38971e413b9ee6b0902bd6cf1fc58bce
Daniel <danielgulic@gmail.com>
2021-06-25 23:20:53 +1000

    add prisma as workspace dep

--- default.nix
+++ default.nix
@@ -6,6 +6,7 @@ mkYarnPackage {
   packageJSON = ./package.json;
   yarnLock = ./yarn.lock;
   yarnNix = ./yarn.nix;
-  patchPhase = "${pkgs.tree}/bin/tree 2>&1; node_modules/.bin/prisma generate";
+	workspaceDependencies = ["prisma"];
+  patchPhase = "${pkgs.tree}/bin/tree 2>&1; prisma generate";
   buildPhase = "yarn build:nix";
 }
